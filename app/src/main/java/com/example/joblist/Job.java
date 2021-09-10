1de8d9c7c2cabbbe0a3ab70042f6f73e8a3b5c6d
thirza258 <thirzahmad@gmail.com>
2021-09-10 20:38:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/joblist/Job.java
@@ -0,0 +1,5 @@
+package com.example.joblist;
+
+public class Job {
+
+}
---
+++ app/src/main/java/com/example/joblist/JobAdapter.java
@@ -0,0 +1,8 @@
+package com.example.joblist;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class JobAdapter extends ArrayList<> {
+}
